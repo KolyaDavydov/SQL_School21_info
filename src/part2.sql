9385fb97ca36ab782d1b68c5a95aa5bfa7333996
Tania Kiara <taniakia@va-d5.kzn.21-school.ru>
2023-08-22 17:58:05 +0300

    implemented add_verter_check procedure

--- src/part2.sql
+++ src/part2.sql
@@ -20,7 +20,7 @@ BEGIN
             (SELECT max(Checks.id) FROM Checks WHERE Peer = checking AND Task = task_name),
             checker,
             p2p_status,
-            now()
+            time
         );
     ELSE
         INSERT INTO P2P VALUES (
@@ -28,8 +28,34 @@ BEGIN
             (SELECT "Check" FROM P2P WHERE CheckingPeer = checker AND State = 'Start'),
             checker,
             p2p_status,
-            now()
+            time
         );
     END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE add_verter_check(
+    IN checking       VARCHAR,
+    IN task_name      VARCHAR,
+    IN verter_status  CHECK_STATUS,
+    IN "time"         TIME
+)
+AS $$
+BEGIN
+    WITH last_successfull_check AS (
+        SELECT "Check", max(P2P.time)
+        FROM P2P
+        JOIN Checks AS c
+            ON  c.Task = task_name
+            AND c.Peer = checking
+        WHERE P2P.state = 'Success'
+        GROUP BY "Check"
+    )
+    INSERT INTO Verter VALUES (
+        (SELECT count(*) + 1 FROM Verter),
+        (SELECT "Check" FROM last_successfull_check),
+        verter_status,
+        time
+    );
 END;
 $$ LANGUAGE plpgsql;
