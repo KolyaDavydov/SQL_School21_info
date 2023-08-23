157e96a0c15821aaf23a1c80b338d6499ce62765
nohoteth <nohoteth@student.21-school.ru>
2023-08-23 16:48:11 +0300

    rework part 3 04-06

--- src/part3/part3.sql
+++ src/part3/part3.sql
@@ -41,33 +41,84 @@ SELECT *
 FROM fnc_noexit_peers_ondate('2023-02-01');
 
 -- 4) Процедура для подсчёта изменений в количестве пир поинтов каждого пира по таблице TransferredPoints
--- TODO дописать с запросом
-CREATE OR REPLACE PROCEDURE peer_points_change(
-    INOUT Peer varchar DEFAULT '',
-    INOUT PointsChange INTEGER DEFAULT 0
-    ) AS $$
-BEGIN
-  SELECT checkingpeer, pointsamount FROM transferredpoints
-  INTO Peer, PointsChange;
-END
-$$ LANGUAGE plpgsql;
-CALL peer_points_change();
 
--- Запрос для процедуры
-SELECT Peer, sum(PointsChange) AS PointsChange
-FROM (
-    SELECT checkingpeer AS Peer, pointsamount AS PointsChange FROM transferredpoints
-UNION
-SELECT checkedpeer AS Peer, 0-pointsamount AS PointsChange FROM transferredpoints) AS f 
-GROUP BY Peer
-ORDER BY PointsChange DESC;
+CREATE OR REPLACE PROCEDURE proc_peer_points_change(res REFCURSOR) 
+AS $$
+BEGIN
+    OPEN res FOR
+    SELECT Peer, sum(PointsChange) AS PointsChange
+        FROM (
+            SELECT checkingpeer AS Peer, pointsamount AS PointsChange FROM transferredpoints
+        UNION
+        SELECT checkedpeer AS Peer, 0-pointsamount AS PointsChange FROM transferredpoints) AS f 
+        GROUP BY Peer
+        ORDER BY PointsChange DESC;
+END;
+$$ LANGUAGE plpgsql;
+BEGIN; 
+CALL proc_peer_points_change('res');
+FETCH ALL FROM "res";
+END;
 
 -- 5) Посчитать изменение в количестве пир поинтов каждого пира по таблице, возвращаемой первой функцией из Part 3
--- Запрос для процедуры
-SELECT Peer1 AS Peer, SUM(pointsamount) AS PointsChange FROM fnc_transferred_points()
+-- TODO расходятся результаты с частью 4
+CREATE OR REPLACE PROCEDURE proc_peer_points_change_by_func(res REFCURSOR) 
+AS $$
+BEGIN
+    OPEN res FOR
+    SELECT Peer1 AS Peer, SUM(pointsamount) AS PointsChange FROM fnc_transferred_points()
 GROUP BY fnc_transferred_points.Peer1
 ORDER BY PointsChange DESC;
+END;
+$$ LANGUAGE plpgsql;
+
+-- BEGIN; 
+-- CALL proc_peer_points_change_by_func('res');
+-- FETCH ALL FROM "res";
+-- END;
+
 -- 6) Определить самое часто проверяемое задание за каждый день
--- Запрос для процедуры
-SELECT date AS Day, task AS Task, COUNT(task) FROM checks
-GROUP BY date, task;
+
+CREATE OR REPLACE PROCEDURE proc_peer_points_change_by_func(res REFCURSOR) 
+AS $$
+BEGIN
+    OPEN res FOR
+        SELECT date AS Day, task AS Task, COUNT(task) 
+        FROM checks
+        GROUP BY date, task;
+END;
+$$ LANGUAGE plpgsql;
+
+-- BEGIN; 
+-- CALL proc_peer_points_change_by_func('res');
+-- FETCH ALL FROM "res";
+-- END;
+
+select date, task, count(2) cnt
+  from checks
+ group by date, task
+ having (date,count(1))=
+  (
+   select date, count(1) from checks
+    where date =(select date from checks group by date order by count(1) desc limit 1)
+    group by date, task
+    order by count(1) desc
+)
+
+-- 7) Найти всех пиров, выполнивших весь заданный блок задач и дату завершения последнего задания
+
+CREATE OR REPLACE PROCEDURE proc_peer_closed_block(task_block varchar, res REFCURSOR) 
+AS $$
+BEGIN
+    OPEN res FOR
+        SELECT title 
+        FROM tasks
+        WHERE title ~ CONCAT('^', task_block, '[0-9]+_');
+END;
+$$ LANGUAGE plpgsql;
+
+-- BEGIN; 
+-- CALL proc_peer_closed_block('SQL', 'res');
+-- FETCH ALL FROM "res";
+-- END;
+
