d93670d937c32730d553002a1660342860c4fec6
nohoteth <nohoteth@student.21-school.ru>
2023-08-25 22:12:18 +0300

    add ex 4 part 4

--- src/part4/part4.sql
+++ src/part4/part4.sql
@@ -25,10 +25,10 @@ $$ LANGUAGE plpgsql;
 
 -- 3) Создать хранимую процедуру с выходным параметром, которая уничтожает все SQL DML триггеры в текущей базе данных. 
 -- Выходной параметр возвращает количество уничтоженных триггеров.
-CREATE OR REPLACE PROCEDURE proc_remove_all_triggers(trg_count INTEGER) 
+
+CREATE OR REPLACE PROCEDURE proc_remove_all_triggers(INOUT trigger_count INTEGER) 
 AS $$
 DECLARE
-    trigger_count INTEGER;
     table_rec record;
 BEGIN
    for table_rec in (SELECT  event_object_table AS table_name ,trigger_name         
@@ -36,14 +36,42 @@ BEGIN
                     GROUP BY table_name , trigger_name 
                     ORDER BY table_name ,trigger_name)
    loop
-     execute 'DROP TRIGGER "' || table_rec.trigger_name ||'" ON "' || table_rec.table_name || '" CASCADE;'; 
+     execute 'DROP TRIGGER "' || table_rec.trigger_name ||'" ON "' || table_rec.table_name || '" CASCADE;';
+     trigger_count = trigger_count+1;
    end loop;
-   SELECT @trigger_count = COUNT(trigger_name) FROM table_rec;
+   return;
 END;
 $$ LANGUAGE plpgsql;
 
 -- BEGIN; 
--- CALL proc_remove_all_triggers();
+-- CALL proc_remove_all_triggers(0);
 -- END;
+
 -- 4) Создать хранимую процедуру с входным параметром, которая выводит имена и описания типа объектов (только хранимых процедур и скалярных функций), 
 -- в тексте которых на языке SQL встречается строка, задаваемая параметром процедуры.
+-- TODO проверить какая-то фигня с выводом 
+
+CREATE OR REPLACE PROCEDURE proc_search_string_in_proc(needle VARCHAR, res REFCURSOR) 
+AS $$
+BEGIN
+    OPEN res FOR
+            SELECT
+            proname AS Name,
+            CASE prokind WHEN 'p' THEN 'Procedure' 
+            WHEN 'f' THEN 'Function' ELSE null END
+            AS Description
+            FROM pg_catalog.pg_proc pr
+            JOIN pg_catalog.pg_namespace ns ON ns.oid = pr.pronamespace
+            WHERE prosrc ilike '%' || needle || '%'
+            AND nspname != 'pg_catalog'
+            AND nspname != 'information_schema';
+END;
+$$ LANGUAGE plpgsql;
+
+DROP PROCEDURE proc_search_string_in_proc(IN needle VARCHAR, OUT res REFCURSOR);
+BEGIN; 
+CALL proc_search_string_in_proc('select', 'res');
+FETCH ALL FROM "res";
+END;
+
+    
