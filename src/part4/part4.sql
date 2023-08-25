560dca1ac06d55a544bbc67311f12cb47cc8a2b8
nohoteth <nohoteth@student.21-school.ru>
2023-08-25 20:48:13 +0300

    add ex 3 part 4

--- src/part4/part4.sql
+++ src/part4/part4.sql
@@ -11,7 +11,7 @@ BEGIN
                     AND schemaname != 'information_schema'
                     AND tablename ~ '^TableName')
    loop
-     execute 'drop table "'||table_rec.tname||'" cascade';
+     execute 'DROP TABLE "' || table_rec.tname || '" CASCADE';
    end loop;
 END;
 $$ LANGUAGE plpgsql;
@@ -19,3 +19,31 @@ $$ LANGUAGE plpgsql;
 -- BEGIN; 
 -- CALL proc_remove_tables_by_name();
 -- END;
+
+-- 2) Создать хранимую процедуру с выходным параметром, которая выводит список имен и параметров всех скалярных SQL функций пользователя в текущей базе данных. 
+-- Имена функций без параметров не выводить. Имена и список параметров должны выводиться в одну строку. Выходной параметр возвращает количество найденных функций.
+
+-- 3) Создать хранимую процедуру с выходным параметром, которая уничтожает все SQL DML триггеры в текущей базе данных. 
+-- Выходной параметр возвращает количество уничтоженных триггеров.
+CREATE OR REPLACE PROCEDURE proc_remove_all_triggers(trg_count INTEGER) 
+AS $$
+DECLARE
+    trigger_count INTEGER;
+    table_rec record;
+BEGIN
+   for table_rec in (SELECT  event_object_table AS table_name ,trigger_name         
+                    FROM information_schema.triggers  
+                    GROUP BY table_name , trigger_name 
+                    ORDER BY table_name ,trigger_name)
+   loop
+     execute 'DROP TRIGGER "' || table_rec.trigger_name ||'" ON "' || table_rec.table_name || '" CASCADE;'; 
+   end loop;
+   SELECT @trigger_count = COUNT(trigger_name) FROM table_rec;
+END;
+$$ LANGUAGE plpgsql;
+
+-- BEGIN; 
+-- CALL proc_remove_all_triggers();
+-- END;
+-- 4) Создать хранимую процедуру с входным параметром, которая выводит имена и описания типа объектов (только хранимых процедур и скалярных функций), 
+-- в тексте которых на языке SQL встречается строка, задаваемая параметром процедуры.
